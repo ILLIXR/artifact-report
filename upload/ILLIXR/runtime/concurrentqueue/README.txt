df3382166a014e46b34dd926e1272026af7d2380
Samuel Grayson <sam@samgrayson.me>
2021-09-22 16:02:46 -0500

    Refine upload

--- upload/ILLIXR/runtime/concurrentqueue/README.txt
+++
@@ -1,3 +0,0 @@
-See https://github.com/cameron314/concurrentqueue
-
-Simplified BSD License, with the exception of `lightweightsemaphore.hpp`, which is released under the Zlib license.
--- upload/monado_integration/src/xrt/targets/openxr/install_active_runtime.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh -eux
-sysconfdir="$DESTDIR"/"$1"
-manifest="$2"
-xrversion="$3"
-
-runtime_path="$sysconfdir"/xdg/openxr/"$xrversion"/active_runtime.json
-
-mkdir -p "$sysconfdir"/xdg/openxr/"$xrversion"
-ln -s "$manifest" "$runtime_path"
