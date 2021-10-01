759caa8626842ea224fb4517e7913d7aeffeb7c9
Samuel Grayson <sam@samgrayson.me>
2021-09-30 19:06:59 -0500

    Add install/run scripts

---
+++ install_deps.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+
+set -e -x
+cd "$(dirname ""${0}"")"
+
+# Install dependencies
+./ILLIXR/install_deps.sh
+./results/install_deps.sh
+./godot/install_deps.sh
+
+# Build godot
+scons -C godot -j$(nproc) platform=x11 target=release_debug
